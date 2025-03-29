ab822a871544c73b11a3e2c4865355e7d54089c1
Ajay Wakodikar <ajaywakodikarsocial@gmail.com>
2025-03-29 18:00:06 +0530

    Updated main.cpp source file
    Updated main function (definition): It accepts filename as an argument to read and then passes it for further tokenization

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,5 +1,17 @@
-#include <iostream>
+#include "io.hpp"
+#include "helium.hpp"
 
-auto main() -> int {
-    std::cout << "Welcome to Helium!!\n";
+auto main(int argc, char* argv[]) -> int {
+    if(argc < 2) {
+		std::cout << "Please specify a file with the executable to read..\n";
+		std::cout << "For example: <executable-name> <filename>\n";
+
+		return 1;
+	}
+
+	std::string filename = argv[1];
+	std::string contents = read_file(filename);
+	helium_compile(contents);
+    
+	return 0;
 }
