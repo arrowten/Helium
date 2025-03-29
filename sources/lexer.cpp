5fe1ddd1c08480197e8cfa6b8f3c728819120a29
Ajay Wakodikar <ajaywakodikarsocial@gmail.com>
2025-03-29 12:40:46 +0530

    Updated lexer.cpp source file
    Added lexer_advance function (definition)
    Added lexer_skip_whitespaces function (definition)

--- sources/lexer.cpp
+++ sources/lexer.cpp
@@ -9,3 +9,14 @@ auto initialize_lexer(std::string source) -> std::shared_ptr<Lexer> {
 
     return lex;
 }
+
+void lexer_advance(std::shared_ptr<Lexer> lex) {
+    lex->_index++;
+    lex->_c = (lex->_index < lex->_size) ? lex->_source[lex->_index]: '\0';
+}
+
+void lexer_skip_whitespaces(std::shared_ptr<Lexer> lex) {
+    while (lex->_c == '\r' || lex->_c == '\n' || lex->_c == '\t' || lex->_c == ' ') {
+        lexer_advance(lex);
+    }
+}
